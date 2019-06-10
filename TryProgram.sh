c186aaf049d217410e167d2428f6e393e3ccaed7
flaxmans <sam.flaxman@gmail.com>
2019-06-10 13:39:40 -0600

    added a wrapper test script and another example set

---
+++ TryProgram.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+refFile=$1
+
+echo "Base reference file is: $refFile"
+echo  "Testing while loop over string stream:"
+time ./TestBoostZip ${refFile}.bz2 1
+time ./TestBoostZip ${refFile}.gz 1
+time ./TestBoostZip ${refFile} 1
+
+printf "\n\t*******************\n\n"
+
+echo "Testing file writes:"
+# test file writes:
+time ./TestBoostZip ${refFile}.bz2 0 > footestbz2.vcf
+time ./TestBoostZip ${refFile}.gz 0 > footestgz.vcf
+time ./TestBoostZip ${refFile} 0 > footest.vcf
+
+printf "\n\t*******************\n\n"
+
+printf "\nDiffing files.  No output means no diffs.\n"
+for i in footest.vcf footestbz2.vcf footestgz.vcf
+do
+	printf "\n\tdiff $i ${refFile}:"
+	diff $i ${refFile}
+done
+echo " "
